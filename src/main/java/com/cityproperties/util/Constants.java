f463440e82286c0b095ac0dadf66fe4bedfd0c09
Neil Juan <neil.r.juan@gmail.com>
2012-07-01 11:12:12 +0400

    Initial version. Contains constants that is used globally.

---
+++ src/main/java/com/cityproperties/util/Constants.java
@@ -0,0 +1,15 @@
+package com.cityproperties.util;
+
+public class Constants {
+	
+	// Constants
+	public static final String MODEL_CLIENT = "modelClient";
+	public static final String MODEL_BA = "modelBusinessAssociate";
+	public static final String CLIENT = "client";
+	public static final String CLIENTS = "clients";
+	public static final String BA = "businessAssociate";
+	public static final String BAS = "businessAssociates";
+	public static final String MALE = "M";
+	public static final String FEMALE = "F";
+
+}
