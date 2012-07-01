cce309c47d94ea578721dad1531faaa12e2e75c8
Neil Juan <neil.r.juan@gmail.com>
2012-07-01 11:45:10 +0400

    Change member to static. Add a setter for DI.

--- src/main/java/com/cityproperties/util/EncryptPassword.java
+++ src/main/java/com/cityproperties/util/EncryptPassword.java
@@ -4,20 +4,18 @@ import org.jasypt.util.password.BasicPasswordEncryptor;
 
 public class EncryptPassword {
 	
+	private static BasicPasswordEncryptor passwordEncryptor;
+	
 	public static String encrypt(String password) {
-		
-		BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
-		
 		return passwordEncryptor.encryptPassword(password);
-		
 	}
 	
 	public static boolean checkPassword(String plainPassword, String encryptedPassword) {
-		
-		BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
-		
 		return passwordEncryptor.checkPassword(plainPassword, encryptedPassword);
-		
+	}
+
+	public static void setPasswordEncryptor(BasicPasswordEncryptor passwordEncryptor) {
+		EncryptPassword.passwordEncryptor = passwordEncryptor;
 	}
 
 }
